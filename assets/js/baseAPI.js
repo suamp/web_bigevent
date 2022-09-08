55b1c61853f562df6a538d430b1e01ba614e9d7f
samp <1653548567@qq.com>
2022-09-08 16:43:27 +0800

    完成了登录和注册的开发

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,4 @@
+// 注意每次调用$.get()或$.post或$.ajax()的时候会先调用ajaxPrefilter这个函数  在这个函数中 可以拿到我们给Ajax提供的配置对象
+$.ajaxPrefilter(function(options) {
+    options.url = 'http://www.liulongbin.top:3007' + options.url
+})
