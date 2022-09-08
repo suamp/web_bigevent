69b8fcc3bae2a2e8ac2f6bee593f03a4e13086af
samp <1653548567@qq.com>
2022-09-09 00:18:38 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,23 @@
 // 注意每次调用$.get()或$.post或$.ajax()的时候会先调用ajaxPrefilter这个函数  在这个函数中 可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
     options.url = 'http://www.liulongbin.top:3007' + options.url
+
+    //统一为有权限的接口  设置headres 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //全局2B0B980D.png挂载complete回调函数
+    options.complete = function(res) {
+        console.log('执行了complete回调')
+            //在complete回调函数中 可以使用res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.强制清空token
+            localStorage.removeItem('token')
+                // 2.强制跳转到登录页
+            location = '/login.html'
+        }
+    }
 })
