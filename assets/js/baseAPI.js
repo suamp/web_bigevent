1d1777874941029b97437658e9c912dbbd06d742
samp <1653548567@qq.com>
2022-09-12 03:27:02 +0800

    完成个人中心功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -11,8 +11,8 @@ $.ajaxPrefilter(function(options) {
 
     //全局2B0B980D.png挂载complete回调函数
     options.complete = function(res) {
-        console.log('执行了complete回调')
-            //在complete回调函数中 可以使用res.responseJSON 拿到服务器响应回来的数据
+        // console.log('执行了complete回调')
+        //在complete回调函数中 可以使用res.responseJSON 拿到服务器响应回来的数据
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
             // 1.强制清空token
             localStorage.removeItem('token')
